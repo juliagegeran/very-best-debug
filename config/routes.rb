a00401a12b688fb686d809440bb8da50c26a050d
Julia Ran <59769618+juliagegeran@users.noreply.github.com>
2023-11-12 22:49:40 +0000

    done

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   #user details
   get("/users/:username", { :controller => "users", :action => "show" })
   
-  post("/insert_user_record", { :controller => "venues", :action => "create" })
+  #insert new username
+  post("/insert_user_record", { :controller => "users", :action => "create" })
 
   #update user info
   post("/update_users/:old_username", { :controller => "users", :action => "update" })
@@ -17,16 +18,18 @@ Rails.application.routes.draw do
   #list of all venues
   get("/venues", { :controller => "venues", :action => "index" })
 
+  #create new venue
   post("/insert_venue_record", { :controller => "venues", :action => "create" })
 
   #venue details
   get("/venues/:venue_id", { :controller => "venues", :action => "show" })
 
-  post("/update_venue/:the_id", { :controller => "application", :action => "update" })
+  #update venue details
+  post("/update_venue/:the_id", { :controller => "venues", :action => "update" })
 
   #delete venue
   get("/delete_venue/:id_to_delete", { :controller => "venues", :action => "destroy" })
-  
+
   #insert comment
   post("/insert_comment_record", { :controller => "comments", :action => "create" })
 end
